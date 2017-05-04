a8e2fed5dc418e48b5df7e25ba7f185976250c44
Alisher778 <internet-docs@mail.ru>
2017-05-03 23:45:42 -0400

    styled nav-bar and added home page

--- javascript/main.js
+++ javascript/main.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
 	$('.menu-bar').click(function(){
 	
 		$('.nav-bar').addClass('show-menu-bar');
+		$('.close-bar').click(function(){
+			$('.nav-bar').removeClass('show-menu-bar');
+		})
 	})
 })
