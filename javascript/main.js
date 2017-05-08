421cf0da5961f951af534301f49f93206cc750dd
Alisher778 <internet-docs@mail.ru>
2017-05-07 21:59:31 -0400

    finished sen a broef page

--- javascript/main.js
+++ javascript/main.js
@@ -5,5 +5,24 @@ $(document).ready(function(){
 		$('.close-bar').click(function(){
 			$('.nav-bar').removeClass('show-menu-bar');
 		})
+	});
+
+	$('form').submit(function(e){
+		e.preventDefault();
+		var data = $('form').serializeArray();
+		var name = data[0].value;
+		var email = data[1].value;
+		var phone = data[2].value;
+		var message = data[3].value;
+		
+
+		var email_options = {name: name, email: email, phone: phone, message: message};
+
+	 	emailjs.send("default_service","template_WARSTBE4", email_options)
+	 	$("form")[0].reset();
+	 	$('.message').css('animation', 'show 5s', function(){
+	 		$(this).css('animation', 'none')
+	 	})
+		
 	})
-})
+}) 
