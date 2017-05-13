9ba49427418dc6d55fe75483f143836a4ef0677f
Alisher778 <alisher.musurmonov89@gmail.com>
2017-05-13 19:47:09 -0400

    added coming soon page

--- javascript/main.js
+++ javascript/main.js
@@ -18,20 +18,26 @@ $(document).ready(function(){
 	$(document).scroll(function(){
 	  	var deviceWidth = $(document).width();
 		var scrollTop = $(document).scrollTop();
-		console.log(scrollTop)
-	   
-
+	
 	    if(deviceWidth < 1280 && scrollTop > 100){
-	  
+	  	
 	    	$('nav').css('background-color', '#027369');
 	    	$('.work-page li').slice(-3).hide();
 	    	$('.load-more').css('display', 'flex');
-	    
+	    	
+
 	    }else{
 	    		$('nav').css('background-color', 'transparent')
 	    	}
   	});
 
+	// On click display Coming soon message
+	$('.load-more').click(function(){
+		$('.coming-soon').css('animation', 'show 3s');
+		// remove this animation after 3s
+		setTimeout(function(){$('.coming-soon').removeAttr('style')}, 3000)
+	    		
+	});
 
 	// Send email with EmailJS
 
@@ -47,14 +53,18 @@ $(document).ready(function(){
 
 		var email_options = {name: name, email: email, phone: phone, message: message};
 
-	 	emailjs.send("default_service","template_WARSTBE4", email_options)
+	 	emailjs.send("default_service","template_WARSTBE4", email_options);
+	 	// after submit reset form
 	 	$("form")[0].reset();
-	 	$('.message').css('animation', 'show 5s', function(){
-	 		$(this).css('animation', 'none')
-	 	})
+	 	// display animation
+	 	$('.message').css('animation', 'show 5s');
+	 	// remove this animation after 3s
+	 	setTimeout(function(){$('.coming-soon').removeAttr('style')}, 3000);
 		
-	})
+	});
 
+
+	// On arrow click Scroll top animation
 	$("#scrol-btn").click(function() {
   		$("html, body").animate({ scrollTop: 0 }, 2000);
   		return false;
